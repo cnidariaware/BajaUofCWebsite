1bf68aeb188007157f83989c7dc8a25bb104b885
Sarim-Sheikh-2003 <sheikh.sarim@ucalgary.ca>
2024-01-03 15:40:30 -0700

    Worked on Gallery Page

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,5 +1,60 @@
+import Modal from 'react-modal';
+import { useState } from "react";
+import './Gallery.css';
+
+const images = [
+  { src: 'https://i.pinimg.com/736x/70/28/0f/70280f1b75ea956f4fdef7e31336d9f0.jpg', alt: 'Image 1' },
+  { src: 'https://wallpapers.com/images/featured-full/cool-anime-6kbwj9794wpnsfr1.jpg', alt: 'Image 2' },
+  { src: 'https://wallpapers.com/images/featured/cool-anime-pfp-pictures-71urutpksg13vkbk.jpg', alt: 'Image 3' },
+  { src: 'https://i.pinimg.com/236x/ab/dc/52/abdc52db893674a7b5dcb767be8776e5.jpg', alt: 'Image 4' },
+  { src: 'https://qph.cf2.quoracdn.net/main-qimg-a3fdbac7bf9f8db58f147887ffa76d88-lq', alt: 'Image 5' }
+];
+
 export default function Gallery() {
+    const [open, setOpen] = useState(false)
+    const [display, setDisplay] = useState('')
+    
+    const handleClick = (arg) => {
+        console.log("Test")
+        setDisplay(arg)
+        setOpen(true)
+        console.log(open)
+    }
+
     return (
-        <p>Gallery</p>
+        <div>
+            <div className="gallery">
+                {images.map((image, index) => (
+                    <img
+                    key={index}
+                    className="galleryItem"
+                    src={image.src}
+                    alt={image.alt}
+                    onClick={() => handleClick(image.src)}
+                    />
+                ))}
+            </div>
+            <div>
+                    <Modal
+                        isOpen={open}
+                        onRequestClose={() => setOpen(false)}
+                        style={{
+                            overlay: {
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            },
+                            content: {
+                                position: 'initial'
+                            }
+                        }}
+                    >   
+                        <div className="container">
+                            <button className="modalButton" onClick={() => setOpen(false)}>x</button>
+                            <img className="modalImage" src={display} onClick={() => setOpen(false)} />
+                        </div>
+                    </Modal>
+            </div>
+        </div>
     );
-};
+}
