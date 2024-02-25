7fcbe8d70979ba49cb13f8afe1ad447a0989e82e
Prithish123 <prithish.univ@gmail.com>
2024-02-24 17:19:07 -0700

    init Upcoming Events

--- src/Club Membership & Upcoming Events/UpcominEvents/UpcomingEvents.js
+++ src/Club Membership & Upcoming Events/UpcominEvents/UpcomingEvents.js
@@ -1,5 +1,43 @@
-export default function UpcomingEvents() {
-    return (
-        <p>Upcoming Events</p>
-    );
+const UpcomingEvents = () => {
+  return (
+    <div>
+      <h2>Upcoming Events</h2>
+      <div>
+        <h4>Gorman - California</h4>
+        <h4>25th - 29th April</h4>
+        <p>
+          Nestled in the rugged terrain of Gorman, California, our challenge
+          unfolds amidst a landscape as diverse as it is demanding. From rocky
+          outcrops to dense forests, and even water crossings that test the
+          limits of both man and machine, this terrain offers no quarter to the
+          unprepared. Every twist and turn presents a new obstacle to overcome,
+          a new opportunity to showcase resilience and adaptability. It's a
+          playground for the adventurous, a canvas for those who dare to push
+          the boundaries of exploration. In Gorman, the terrain isn't just a
+          backdrop; it's a character in its own right, shaping the very essence
+          of our off-road pursuits and inspiring us to reach new heights of
+          ingenuity and skill.
+        </p>
+      </div>
+      <div>
+        <h4>Williamsport - Pennsylvania</h4>
+        <h4>13th - 19th May</h4>
+        <p>
+          In the heart of Williamsport, Pennsylvania, our challenge unfolds
+          amidst a dynamic landscape that demands both versatility and
+          resilience. With its rolling hills, dense forests, and occasional
+          waterways, this terrain presents a constant test for both adventurer
+          and vehicle alike. Each twist and turn in the path brings a fresh
+          obstacle to overcome, a new opportunity to showcase adaptability and
+          determination. Williamsport's terrain isn't just scenery; it's an
+          active participant in our off-road pursuits, shaping the very essence
+          of our adventures and inspiring us to continually push our limits. In
+          this rugged environment, exploration is boundless, and our pursuit of
+          innovation and expertise knows no bounds.
+        </p>
+      </div>
+    </div>
+  );
 };
+
+export default UpcomingEvents;
