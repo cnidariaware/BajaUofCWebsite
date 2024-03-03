30ce146c2b8aa6a4db447f9de7ef24bebeb4b088
darkicewolf50 <brock.tomlinson@ucalgary.ca>
2024-03-02 17:04:13 -0700

    init sub teams

---
+++ src/Teams/SubTeams.js
@@ -0,0 +1,34 @@
+const SubTeams = () => {
+	// const jsonImport = (filePath) => {
+	// 	data = import(filePath);
+	// 	return data;
+	// };
+	// leadership_json = jsonImport("./Leadership.json");
+	// console.log(leadership_json);
+	return (
+		<div>
+			<div>
+				<h2>Leadership</h2>
+				<p>gallery</p>
+			</div>
+			<table>
+				<tbody>
+					<tr>
+						<td>Aidan Schroeder</td>
+						<td>Team Captain</td>
+					</tr>
+					<tr>
+						<td>Hewitt Yee</td>
+						<td>Drivetrain Lead</td>
+					</tr>
+					<tr>
+						<td>Nole Tabelon</td>
+						<td>Chassis Lead</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default SubTeams;
