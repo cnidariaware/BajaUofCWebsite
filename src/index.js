27fe57c40e1f2ba0ee2906682179264be107aab2
darkicewolf50 <brock.tomlinson@ucalgary.ca>
2024-03-17 21:12:02 -0600

    removed become a sponsor

--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './Header/Header';
-import AboutUs from './AboutUs/AboutUs';
-import Teams from './Teams/Teams';
-import OurSponsors from './OurSponsors/OurSponsors';
-import BecomeASponsor from './BecomeASponsor/BecomeASponsor';
-import JoinTheClub from './Club Membership & Upcoming Events/JoinTheClub/JoinTheClub';
-import UpcomingEvents from './Club Membership & Upcoming Events/UpcominEvents/UpcomingEvents';
-import Gallery from './Gallery/Gallery';
-import './index.css';
+import Header from "./Header/Header";
+import AboutUs from "./AboutUs/AboutUs";
+import Teams from "./Teams/Teams";
+import OurSponsors from "./OurSponsors/OurSponsors";
+import JoinTheClub from "./Club Membership & Upcoming Events/JoinTheClub/JoinTheClub";
+import UpcomingEvents from "./Club Membership & Upcoming Events/UpcominEvents/UpcomingEvents";
+import Gallery from "./Gallery/Gallery";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element = {<Header />}>
-          <Route path = '/' element = {<AboutUs />}></Route>
-          <Route path = '/Teams' element = {<Teams />}></Route>
-          <Route path = '/OurSponsors' element = {<OurSponsors />}></Route>
-          <Route path = '/BecomeASponsor' element = {<BecomeASponsor />}></Route>
-          <Route path = '/JoinTheClub' element = {<JoinTheClub />}></Route>
-          <Route path = '/UpcomingEvents' element = {<UpcomingEvents />}></Route>
-          <Route path = '/Gallery' element = {<Gallery />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Header />}>
+					<Route
+						path="/"
+						element={<AboutUs />}></Route>
+					<Route
+						path="/Teams"
+						element={<Teams />}></Route>
+					<Route
+						path="/OurSponsors"
+						element={<OurSponsors />}></Route>
+					<Route
+						path="/JoinTheClub"
+						element={<JoinTheClub />}></Route>
+					<Route
+						path="/UpcomingEvents"
+						element={<UpcomingEvents />}></Route>
+					<Route
+						path="/Gallery"
+						element={<Gallery />}></Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
