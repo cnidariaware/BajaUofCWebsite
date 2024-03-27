58f2ad739c5e7df528550de5e7b055a0f85e7229
darkicewolf50 <brock.tomlinson@ucalgary.ca>
2024-03-26 22:32:28 -0600

    added header: added dark mode css and togglable dark/light mode and checks for browser preferance

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,5 @@
 import logo from "./logo.png";
+import lightDark from "./light-dark.webp";
 import { Outlet, Link } from "react-router-dom";
 import "./Header.css";
 
@@ -7,25 +8,62 @@ import "./Header.css";
  * @returns {JSX.Element} JSX - HTML tags and JS functionality
  * @description The top header part of the page includes the naviagtion
  * @author Brock <darkicewolf50@gmail.com>
- * @todo convert any dropdowns and use <Link> just like <a> https://github.com/Akshpreet02/EventSphere
+ * @todo final css
  */
 const Header = () => {
+	/**
+	 * @param {null} null -  Takes in nothing
+	 * @returns {CSSStyleRule} CSS - changes page to darkmode
+	 * @description inverts all of the colors of body without touching the pictures
+	 * @author Brock <darkicewolf50@gmail.com>
+	 */
+	const switchDarkMode = () => {
+		const body = document.getElementById("root");
+		const logoCss = document.getElementById("logo");
+
+		body.classList.toggle("darkmode");
+		logoCss.classList.toggle("logoAfterDark");
+	};
+
+	/**
+	 * @param {null} null -  Takes in nothing
+	 * @returns {CSSStyleRule} CSS - makes it compliant with bowser preferances
+	 * @description checks for what the browser prefers
+	 * @author Brock <darkicewolf50@gmail.com>
+	 */
+	document.addEventListener("DOMContentLoaded", () => {
+		const prefersDarkMode =
+			window.matchMedia &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches;
+		if (prefersDarkMode) {
+			switchDarkMode();
+		}
+	});
+
 	return (
 		<>
 			<header>
-				<Link to={"/"}>
-					<figure>
+				<div>
+					<Link to={"/"}>
+						<figure>
+							<img
+								id="logo"
+								src={logo}
+								alt="Schulich Off-Road's logo"
+							/>
+							<figcaption>
+								<h1>Schulich Offroad</h1>
+							</figcaption>
+						</figure>
+					</Link>
+					<button onClick={switchDarkMode}>
 						<img
-							id="logo"
-							src={logo}
-							alt="Schulich Off-Road's logo"
+							id="darkModeToggle"
+							src={lightDark}
+							alt="Light/Dark Toggle Symbol"
 						/>
-						<figcaption>
-							<h1>Schulich Offroad</h1>
-						</figcaption>
-					</figure>
-				</Link>
-
+					</button>
+				</div>
 				<nav>
 					<ul>
 						<Link to={"/"}>
