079c6908ef32868074fd8594a3d756db978eface
darkicewolf50 <darkicewolf50@gmail.com>
2025-04-26 14:22:56 -0600

    feat(dev): css errors fixed, and homepage set up before realizing that there was a homepage branch

---
+++ src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import UpdateBanner from "../Header/UpdateBanner";
+
+export default function AboutsUs() {
+	return (
+		<>
+			<UpdateBanner
+				updatedTitleText="UCalgary Baja"
+				updatedSubtitleText=""
+				updatedImgUrl="https://picsum.photos/200"
+				updatetdImgAlt="Lorem Picsum"
+			/>
+		</>
+	);
+}
