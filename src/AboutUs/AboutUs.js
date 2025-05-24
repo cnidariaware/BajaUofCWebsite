75c05cf265b36517247c2e571308e61a028eb158
darkicewolf50 <darkicewolf50@gmail.com>
2025-05-24 12:12:11 -0600

    feat(about): final missing html as a temporary, css for about us, subteams finished, leadership almost finished, todo rest of team

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -1,4 +1,5 @@
 import UpdateBanner from "../Header/UpdateBanner";
+import "./AboutUs.css";
 
 export default function AboutsUs() {
 	let subteam_info = [
@@ -124,6 +125,21 @@ export default function AboutsUs() {
 			lead_des: "I am a third year mechanical engineering major.",
 		},
 	];
+
+	let member_info = [
+		"temp1",
+		"temp2",
+		"temp3",
+		"temp4",
+		"temp5",
+		"temp6",
+		"temp7",
+		"temp8",
+		"temp9",
+		"temp10",
+		"temp11",
+		"temp12",
+	];
 	return (
 		<>
 			<UpdateBanner
@@ -134,104 +150,107 @@ export default function AboutsUs() {
 			/>
 			<div id="About">
 				<div>
-					<h3>ABOUT US</h3>
-					<p>
-						UCalgary Baja is a competitive group of talented and determined
-						engineering students striving to expand their horizons, earn
-						experience, and network with leading companies in their respective
-						fields. Our team comprises 6 subteams: Drivetrain, Logistics,
-						Suspension, Chassis and Ergonomics, Steering, and Electrical and
-						Testing. Each subteam is mentored by one of our talented leads.
-					</p>
-					<p>
-						In their first year on UCalgary Baja, junior members are a vital
-						part of our team. This year, we recruited 17 new members onto the
-						team during our recruitment process. This big addition allows the
-						team to provide an enhanced educational experience for more students
-						than ever before. Adding junior members allows us to complete bigger
-						projects and increase the amount of in-house fabrication we can do,
-						reducing our reliance on outside sponsors and contractors.
-					</p>
+					<div id="intro">
+						<h3>ABOUT US</h3>
+						<p>
+							UCalgary Baja is a competitive group of talented and determined
+							engineering students striving to expand their horizons, earn
+							experience, and network with leading companies in their respective
+							fields. Our team comprises 6 subteams: Drivetrain, Logistics,
+							Suspension, Chassis and Ergonomics, Steering, and Electrical and
+							Testing. Each subteam is mentored by one of our talented leads.
+						</p>
+						<p>
+							In their first year on UCalgary Baja, junior members are a vital
+							part of our team. This year, we recruited 17 new members onto the
+							team during our recruitment process. This big addition allows the
+							team to provide an enhanced educational experience for more
+							students than ever before. Adding junior members allows us to
+							complete bigger projects and increase the amount of in-house
+							fabrication we can do, reducing our reliance on outside sponsors
+							and contractors.
+						</p>
 
-					<p>
-						We at UCalgary Baja foster an environment of personal growth and try
-						our best to ensure that new members are included in all aspects of
-						the team. Each new member takes the lead on a junior project, which
-						includes designing, testing, and manufacturing a crucial part of the
-						build. This sets us apart from other teams who do not necessarily
-						give out design work to new members.
-					</p>
+						<p>
+							We at UCalgary Baja foster an environment of personal growth and
+							try our best to ensure that new members are included in all
+							aspects of the team. Each new member takes the lead on a junior
+							project, which includes designing, testing, and manufacturing a
+							crucial part of the build. This sets us apart from other teams who
+							do not necessarily give out design work to new members.
+						</p>
 
-					<p>
-						This past year, a junior member learned how to use a four-axis CNC
-						machine to make a custom u-joint that performed admirably in
-						competition. We aim to provide a fulfilling experience to new
-						members that will complement their in-class education and encourage
-						them to develop into capable engineers and proven leaders with the
-						skills and knowledge that employers need and want. We strive to
-						bridge the gap between education and application by providing
-						experience in a fun and memorable way.
-					</p>
-				</div>
-				<div>
-					<div>
-						<h4>THE NUMBERS</h4>
-					</div>
-					<div>
-						<h5>STUDENTS</h5>
-						<h6>48</h6>
 						<p>
-							Our team is composed of 48 bold engineering students who wish to
-							expand their skills, test their limits, and challenge their
-							abilities.
-						</p>
-					</div>
-					<div>
-						<h5>GROWTH</h5>
-						<h6>200%</h6>
-						<p>
-							Our team is exploding this year with over double the size compared
-							to last year.
-						</p>
-					</div>
-					<div>
-						<h5>SUBTEAMS</h5>
-						<h6>8</h6>
-						<p>
-							We have 7 subteams on our team. This encourages students to pick a
-							segment in the group they strive in, or challenge themselves to
-							learn something new.
-						</p>
-					</div>
-					<div>
-						<h5>FOUNDED</h5>
-						<h6>1990</h6>
-						<p>
-							Out team has stood the test of time. Founded in 1990, we have
-							years of experience helping others who love a challenge find a
-							place to spread their wings and test how far they can go.
-						</p>
-					</div>
-					<div>
-						<h5>SCHOOL YEARS</h5>
-						<h6>4</h6>
-						<p>
-							On average, our team members spend 4 years with us, being able to
-							experience everything our team has to offer, from competitions to
-							manufacturing and the logistics of operation.
-						</p>
-					</div>
-					<div>
-						<h5>COMPETITION RANKING</h5>
-						<h6>Top 30</h6>
-						<p>
-							Our team is one of the leading BAJA SAE teams in Canada, each year
-							we strive to reach newer heights and higher rankings, joining us
-							on the adventure of a lifetime!
+							This past year, a junior member learned how to use a four-axis CNC
+							machine to make a custom u-joint that performed admirably in
+							competition. We aim to provide a fulfilling experience to new
+							members that will complement their in-class education and
+							encourage them to develop into capable engineers and proven
+							leaders with the skills and knowledge that employers need and
+							want. We strive to bridge the gap between education and
+							application by providing experience in a fun and memorable way.
 						</p>
 					</div>
 				</div>
-				<div>
+				<div id="club-stats">
+					<h4>THE NUMBERS</h4>
+					<div>
+						<div>
+							<h5>STUDENTS</h5>
+							<h6>48</h6>
+							<p>
+								Our team is composed of 48 bold engineering students who wish to
+								expand their skills, test their limits, and challenge their
+								abilities.
+							</p>
+						</div>
+						<div>
+							<h5>GROWTH</h5>
+							<h6>200%</h6>
+							<p>
+								Our team is exploding this year with over double the size
+								compared to last year.
+							</p>
+						</div>
+						<div>
+							<h5>SUBTEAMS</h5>
+							<h6>8</h6>
+							<p>
+								We have 7 subteams on our team. This encourages students to pick
+								a segment in the group they strive in, or challenge themselves
+								to learn something new.
+							</p>
+						</div>
+						<div>
+							<h5>FOUNDED</h5>
+							<h6>1990</h6>
+							<p>
+								Out team has stood the test of time. Founded in 1990, we have
+								years of experience helping others who love a challenge find a
+								place to spread their wings and test how far they can go.
+							</p>
+						</div>
+						<div>
+							<h5>SCHOOL YEARS</h5>
+							<h6>4</h6>
+							<p>
+								On average, our team members spend 4 years with us, being able
+								to experience everything our team has to offer, from
+								competitions to manufacturing and the logistics of operation.
+							</p>
+						</div>
+						<div>
+							<h5>COMPETITION RANKING</h5>
+							<h6>Top 30</h6>
+							<p>
+								Our team is one of the leading BAJA SAE teams in Canada, each
+								year we strive to reach newer heights and higher rankings,
+								joining us on the adventure of a lifetime!
+							</p>
+						</div>
+					</div>
+				</div>
+				<div id="about-subteams">
 					<div>
 						<h3>Which Sub Team Will You Choose?</h3>
 						<p>Take a Closer Look at Our Sub Teams</p>
@@ -243,7 +262,7 @@ export default function AboutsUs() {
 								<div key={subteam_index}>
 									{subteam_index % 2 !== 0 ? (
 										<>
-											<div className={"About" + (subteam_index % 2)}>
+											<div>
 												<img
 													src={subteam.subteam_pic}
 													alt={subteam.subteam_pic_alt}
@@ -273,7 +292,7 @@ export default function AboutsUs() {
 						})}
 					</div>
 				</div>
-				<div>
+				<div id="leadership">
 					<div>
 						<h4>Our Leads</h4>
 						<p>
@@ -298,21 +317,30 @@ export default function AboutsUs() {
 							);
 						})}
 					</div>
+				</div>
+				<div>
 					<div>
-						<div>
-							<h4>Meet the Rest of the Team</h4>
-							<p>
-								This is the rest of our amazing Baja team, each of the following
-								members are part of different subteams, mentored by our leads
-								above. They are all enthusiastic, hard working, and dedicated
-								members of our team.
-							</p>
-						</div>
-						<div>
-							<img alt="test" />
-							<p>Name</p>
-							<p>Subteam on</p>
-						</div>
+						<h4>Meet the Rest of the Team</h4>
+						<p>
+							This is the rest of our amazing Baja team, each of the following
+							members are part of different subteams, mentored by our leads
+							above. They are all enthusiastic, hard working, and dedicated
+							members of our team.
+						</p>
+					</div>
+					<div>
+						{Object.values(member_info).map((info) => {
+							return (
+								<div key={info}>
+									<img
+										src="https://picsum.photos/200/200"
+										alt="test"
+									/>
+									<p>Name: {info}</p>
+									<p>Subteam on</p>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
