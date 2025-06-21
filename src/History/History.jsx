829871f5373264ba2d056099df88591544b53911
darkicewolf50 <darkicewolf50@gmail.com>
2025-06-21 15:55:46 -0600

    feat(history): done, need content and mobile layout

--- src/History/History.jsx
+++ src/History/History.jsx
@@ -2,6 +2,7 @@ import UpdateBanner from "../Header/UpdateBanner";
 import "./History.css";
 
 import { Link } from "react-router-dom";
+import React from "react";
 
 export default function History() {
 	let historyTimeLineInfo = [
@@ -34,52 +35,74 @@ export default function History() {
 				<p>
 					The history of our cars are below. They include overall competition
 					results (as well as old competition stats if u wanna include that).
-					Learn more about what we do at competitoin here.
+					Learn more about what we do at competition here.
 				</p>
 				<Link to={"/AboutUs"}>
 					<button>Learn More</button>
 				</Link>
 			</div>
 			<table>
-				{historyTimeLineInfo.map((yearInfo, index) => {
-					return (
-						<tr key={index}>
-							{index % 2 === 0 ? (
-								<>
-									<td>
-										<img
-											src={yearInfo.competitionCarImg}
-											alt={yearInfo.year + "'s Car"}
-										/>
-									</td>
-									<td>{/* Star or circle spot */}*</td>
-									<td>
-										<CompetitionInfo
-											year={yearInfo.year}
-											competitions={yearInfo.competitions}
-										/>
-									</td>
-								</>
-							) : (
-								<>
-									<td>
-										<CompetitionInfo
-											year={yearInfo.year}
-											competitions={yearInfo.competitions}
-										/>
-										<td>{/* Star or circle spot */}*</td>
+				{/* <colgroup>
+					<col />
+					<col class="symbol" />
+					<col />
+				</colgroup> */}
+				<tbody>
+					{historyTimeLineInfo.map((yearInfo, index) => {
+						return (
+							<tr key={index}>
+								{index % 2 === 0 ? (
+									<>
 										<td>
 											<img
 												src={yearInfo.competitionCarImg}
 												alt={yearInfo.year + "'s Car"}
 											/>
 										</td>
-									</td>
-								</>
-							)}
-						</tr>
-					);
-				})}
+
+										<td className="symbol">
+											<span className="dot">{/* Star or circle spot */}</span>
+										</td>
+										<td>
+											<CompetitionInfo
+												year={yearInfo.year}
+												competitions={yearInfo.competitions}
+											/>
+										</td>
+									</>
+								) : (
+									<>
+										<td>
+											<CompetitionInfo
+												year={yearInfo.year}
+												competitions={yearInfo.competitions}
+											/>
+										</td>
+
+										<td className="symbol">
+											<span className="dot">{/* Star or circle spot */}</span>
+										</td>
+										<td>
+											<img
+												src={yearInfo.competitionCarImg}
+												alt={yearInfo.year + "'s Car"}
+											/>
+										</td>
+									</>
+								)}
+							</tr>
+						);
+					})}
+					<tr>
+						<td></td>
+						<td
+							className="symbol"
+							id="HistoryJoin">
+							<div>Where You Join!</div>
+						</td>
+						<td></td>
+					</tr>
+				</tbody>
 			</table>
 		</div>
 	);
@@ -87,23 +110,21 @@ export default function History() {
 
 function CompetitionInfo({ year, competitions }) {
 	return (
-		<>
+		<div
+			key={year + competitions}
+			className="HistoryCompInfo">
 			<h2>{year}</h2>
 			<h4>Overall Competition Results</h4>
 			<ul>
 				{competitions.map((comp, index) => {
 					return (
-						<>
+						<React.Fragment key={comp + index}>
 							<li key={index + "location"}>{comp.location}</li>
 							<li key={index + "place"}>{comp.placement} place</li>
-						</>
+						</React.Fragment>
 					);
 				})}
-				{/* <li>{competitions[0].location}</li>
-				<li>{competitions[0].placement} place</li>
-				<li>{competitions[1].location}</li>
-				<li>{competitions[1].placement} place</li> */}
 			</ul>
-		</>
+		</div>
 	);
 }
